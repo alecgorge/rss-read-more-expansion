a66cc90c8055a5f751c0de4141ea68d765aca844
Alec Gorge <alecgorge@gmail.com>
2016-06-07 10:52:09 -0400

    dockerize

--- Controllers/RssController.cs
+++ Controllers/RssController.cs
@@ -1,37 +1,55 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using System.Xml.Linq;
-using System.Xml.XPath;
 using System.Net.Http;
 using Newtonsoft.Json;
+using StackExchange.Redis;
+using Microsoft.Extensions.Configuration;
 
 namespace rss_expander.Controllers
 {
-    class ReadabilityResponse {
+    class ReadabilityResponse
+    {
         public string content { get; set; }
     }
 
     public class RssController : Controller
     {
+        static HttpClient HttpClient { get; set; } = new HttpClient();
+
+        IDatabase redis { get; }
+
+        public RssController(RedisDb db) {
+            this.redis = db.db;
+        }
+
         public async Task<HttpContent> FetchUrl(string url)
         {
-            using (var client = new HttpClient())
-            using (var response = await client.GetAsync(url))
-            {
-                return response.Content;
-            }
+            var response = await HttpClient.GetAsync(url);
+            return response.Content;
         }
+
+        string CacheKey(string url) {
+            return "rss_expand_" + url;
+        } 
 
         public async Task<string> Readable(string url)
         {
+            var read = await redis.StringGetAsync(CacheKey(url));
+
+            if(read.HasValue) {
+                return read.ToString();
+            }
+
             var encoded = Uri.EscapeUriString(url);
-            var parser = $"http://readability.com/api/content/v1/parser?url=${encoded}&token=dbd37a7736b9e617bed66666c7d2a806395d4e83"
+            var parser = $"http://readability.com/api/content/v1/parser?url={encoded}&token=dbd37a7736b9e617bed66666c7d2a806395d4e83";
             using (var content = await FetchUrl(parser))
             {
                 var json = JsonConvert.DeserializeObject<ReadabilityResponse>(await content.ReadAsStringAsync());
+
+                redis.StringSet(CacheKey(url), json.content, TimeSpan.FromDays(90));
 
                 return json.content;
             }
@@ -41,7 +59,8 @@ namespace rss_expander.Controllers
         [HttpGet, Route("/expanded-rss")]
         public async Task<IActionResult> Get(string url)
         {
-            using (var content = await FetchUrl(url))
+            using (var response = await HttpClient.GetAsync(url))
+            using (var content = response.Content)
             {
                 var doc = XDocument.Load(await content.ReadAsStreamAsync());
 
@@ -62,11 +81,11 @@ namespace rss_expander.Controllers
                         Where(x => x.Name.LocalName == "description").
                         First();
 
-                    descNode.ReplaceNodes(new XCData(await Readable(link)));
+                    var newContent = await Readable(link);
+
+                    descNode.ReplaceAll(new XCData(descNode.Value + "<hr/>" + newContent));
                 }
 
-                
-                
                 return Ok(doc.Declaration.ToString() + doc.ToString());
             }
         }
--- Startup.cs
+++ Startup.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
 namespace rss_expander
 {
+    public class RedisDb {
+        public ConnectionMultiplexer connection { get; }
+
+        public IDatabase db { get; }
+
+        public RedisDb(string url) {
+            connection = ConnectionMultiplexer.Connect(url);
+            db = connection.GetDatabase();
+        }
+    }
+
     public class Startup
     {
         public Startup(IHostingEnvironment env)
@@ -28,14 +41,27 @@ namespace rss_expander
         public void ConfigureServices(IServiceCollection services)
         {
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc(config =>
+            {
+                config.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            });
+
+            services.AddOptions();
+            services.AddSingleton<RedisDb>(new RedisDb(Configuration["REDIS_ADDRESS_INT"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            Console.WriteLine($"[redis] connecting to {Configuration["REDIS_URL"]}");
 
             app.UseMvc();
         }
